32d33b4055f2c41aa78d0fc218e8cf0467c9e156
kunxin-chor <chorkunxin@yahoo.com>
2024-08-12 09:56:07 +0000

    Added example of template variables

--- src/main/java/com/example/demo/controllers/HomeController.java
+++ src/main/java/com/example/demo/controllers/HomeController.java
@@ -1,5 +1,8 @@
 package com.example.demo.controllers;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ResponseBody;
   
@@ -10,5 +13,16 @@ public class HomeController {
     @ResponseBody
     public String helloWorld() {
         return "<h1>Hello World</h1>";
+    }
+
+    @GetMapping("/test")
+    public String test() {
+        return "test";
+    }
+
+    @GetMapping("/test-templates")
+    public String testModel(Map<String, Object> model) {
+        model.put("message", "Goodbye World");
+        return "test-model";
     }
 }
